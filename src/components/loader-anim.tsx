a0a71d9e76404c59ade2658bc9233cf75deda950
José Miguel Oroño <joseomaker@gmail.com>
2025-08-08 11:05:44 -0500

    Create loader-anim.tsx

---
+++ src/components/loader-anim.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import BlocksScaleLoader from '../../assets/blocks-scale-loader.svg?react';
+
+interface LoaderAnimProps {
+  size?: number;
+  className?: string;
+}
+
+const LoaderAnim: React.FC<LoaderAnimProps> = ({ 
+  size = 24, 
+  className = '' 
+}) => {
+  return (
+    <div className={`inline-flex items-center justify-center ${className}`}>
+      <BlocksScaleLoader 
+        width={size}
+        height={size}
+      />
+    </div>
+  );
+};
+
+export default LoaderAnim;
