3c22a3db3b9c4f7370b800a99093c46bc9ce4656
José Miguel Oroño <joseomaker@gmail.com>
2025-08-08 18:56:23 -0500

    Add bin2bool util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function bin2bool(value: number): boolean {
+  return value === 1;
+}
