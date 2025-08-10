de990332a3172a2c3803e99852e5c434ff52f13e
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2025-08-10 16:10:44 -0400

    Refactor routing and update index route
    Replaces the old _index.tsx route file with index.tsx, updating the export to use HomePage. Refactors App.tsx to use react-router-dom's Routes and Route components, adding routes for HomePage, DashboardPage, and ThemeTest.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import ThemeTest from "./app/routes/theme-test";
 function App() {
   return (
     <ThemeProvider
@@ -8,7 +11,11 @@ function App() {
       enableSystem
       disableTransitionOnChange
     >
-      <HomePage />
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/theme-test" element={<ThemeTest />} />
+      </Routes>
     </ThemeProvider>
   );
 }
