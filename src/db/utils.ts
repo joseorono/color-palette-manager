66b45d1600a909ec14c0015df1a9846ac2cf3c98
José Miguel Oroño <joseomaker@gmail.com>
2025-08-11 12:28:27 -0500

    db: Add type anotation to query functions
    Co-Authored-By: Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,55 +1,54 @@
 import { Color, Palette } from '@/types/palette';
 import { db } from "./main";
 
-
 /////////**************
 // Palettes
 /////////**************
 
 // Helper function to get all palettes
-export async function getAllPalettes() {
+export async function getAllPalettes(): Promise<Palette[]> {
     return await db.palettes.toArray();
-  }
-  
-  // Helper function to get palette by id
-  export async function getPaletteById(id: string) {
+}
+
+// Helper function to get palette by id
+export async function getPaletteById(id: string): Promise<Palette | undefined> {
     return await db.palettes.get(id);
-  }
-  
-  // Helper function to update a palette
-  export async function updatePalette(id: string, updates: Partial<Omit<Palette, 'id' | 'createdAt'>>) {
+}
+
+// Helper function to update a palette
+export async function updatePalette(id: string, updates: Partial<Omit<Palette, 'id' | 'createdAt'| 'updatedAt'>>) {
+    const now = new Date();
     return await db.palettes.update(id, {
       ...updates,
-      updatedAt: new Date()
+      updatedAt: now
     });
   }
-  
+
   // Helper function to delete a palette
   export async function deletePalette(id: string) {
     return await db.palettes.delete(id);
   }
-  
+
   /////////**************
   // Colors
   /////////**************
-  
+
   // Helper function to get all colors
-  export async function getAllColors() {
+  export async function getAllColors(): Promise<Color[]> {
     return await db.colors.toArray();
   }
-  
+
   // Helper function to get color by id
-  export async function getColorById(id: string) {
+  export async function getColorById(id: string): Promise<Color | undefined> {
     return await db.colors.get(id);
   }
-  
+
   // Helper function to update a color
   export async function updateColor(id: string, updates: Partial<Omit<Color, 'id'>>) {
     return await db.colors.update(id, updates);
   }
-  
+
   // Helper function to delete a color
   export async function deleteColor(id: string) {
     return await db.colors.delete(id);
   }
-  
