73e835754d32abd9c90f38bcf3cad7bc1f48fb0f
José Miguel Oroño <joseomaker@gmail.com>
2025-08-11 21:16:45 -0500

    Update color constants

--- src/constants/palettes-and-colors.ts
+++ src/constants/palettes-and-colors.ts
@@ -1,8 +1,10 @@
 import { Color } from "@/types/palette";
 
+export const COLOR_ID_LENGTH = 6;
+
 export const DEFAULT_COLORS: Color[] = [
-    { hex: "#1F1F1F", locked: false, name: "Black", role: "foreground" },
-    { hex: "#F5F6FA", locked: false, name: "White", role: "background" },
-    { hex: "#46CEE6", locked: false, name: "Vivid Sky Blue", role: "primary" },
-    { hex: "#1A8499", locked: false, name: "Blue Munsell", role: "secondary" },
+    { id: 'asd111', hex: "#1F1F1F", locked: false, name: "Black", role: "foreground" },
+    { id: 'asd222', hex: "#F5F6FA", locked: false, name: "White", role: "background" },
+    { id: 'asd333', hex: "#46CEE6", locked: false, name: "Vivid Sky Blue", role: "primary" },
+    { id: 'asd444', hex: "#1A8499", locked: false, name: "Blue Munsell", role: "secondary" },
 ]
