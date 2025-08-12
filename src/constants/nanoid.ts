14d78d82262a1db39ff8c724c235ae1375dc4c78
José Miguel Oroño <joseomaker@gmail.com>
2025-08-11 21:45:42 -0500

    Update nanoid.ts

--- src/constants/nanoid.ts
+++ src/constants/nanoid.ts
@@ -7,4 +7,5 @@ export const PALETTE_ID_LENGTH = 12;
 // Use alphanumeric characters for IDs so we can use them in URLs without encoding
 export const ID_DICTIONARY = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-export const nanoid = customAlphabet(ID_DICTIONARY, COLOR_ID_LENGTH);
+export const nanoidColorId = customAlphabet(ID_DICTIONARY, COLOR_ID_LENGTH);
+export const nanoidPaletteId = customAlphabet(ID_DICTIONARY, PALETTE_ID_LENGTH);
