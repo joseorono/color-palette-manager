52cf4ad1f94e6f56bd610987b5ae3843eff91217
Fernando Barrera <barrerafernando54@gmail.com>
2025-09-06 02:21:39 -0400

    app-layout: remove bg-colors

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout() {
   return (
     <ThemeProvider>
       <NuqsAdapter>
-        <main className="mx-auto sm:bg-blue-500 lg:bg-green-500">
+        <main className="mx-auto">
           <AppNavbar />
           <Outlet />
           <Toaster />
