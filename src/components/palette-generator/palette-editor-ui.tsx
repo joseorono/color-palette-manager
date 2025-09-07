b31a90475d6ce683f387425cbc860f1ce9969fcb
Carlos Sierra <carlosmatures@gmail.com>
2025-09-07 15:24:10 -0400

    fix: screen height

--- src/components/palette-generator/palette-editor-ui.tsx
+++ src/components/palette-generator/palette-editor-ui.tsx
@@ -30,7 +30,6 @@ interface PaletteEditorUIProps {
 }
 
 export function PaletteEditorUI({ children }: PaletteEditorUIProps) {
-  const isMobile = useIsMobile();
   const {
     currentPalette,
     savePalette,
@@ -158,7 +157,7 @@ export function PaletteEditorUI({ children }: PaletteEditorUIProps) {
     }
   };
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+    <div className="min-h-fullvh-with-navbar bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       {/* Top bar (responsive inside) */}
       <PaletteNavbar
         onOpenSave={() => setIsSaveOpen(true)}
