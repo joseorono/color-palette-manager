e2baa978c4f04979448732e4b1e202ea6c39657c
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2025-10-14 19:54:42 -0400

    Add PWA asset generation and icons
    Added @vite-pwa/assets-generator as a dev dependency and configured a new npm script 'generate-pwa-assets'. Introduced pwa-assets.config.ts for asset generation settings. Added new favicon and PWA icon assets to the public directory, and updated the main logo image.
    
    Co-Authored-By: José Miguel Oroño <joseomaker@gmail.com>

---
+++ pwa-assets.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "@vite-pwa/assets-generator/config";
+
+export default defineConfig({
+  headLinkOptions: {
+    preset: "2023",
+  },
+  preset: {
+    transparent: {
+      sizes: [32, 128, 180, 192],
+      favicons: [[48, "favicon.ico"]],
+    },
+    maskable: {
+      sizes: [128, 192],
+    },
+    apple: {
+      sizes: [180],
+    },
+  },
+  images: ["public/logo-32x32.png"], // Your source image
+});
